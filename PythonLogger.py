bf502894072acec57372aa4a2118239d971994ee
Anshul Verma <multinucliated@gmail.com>
2020-04-18 19:22:12 +0530

    introduced the new method
    update the function

--- PythonLogger.py
+++ PythonLogger.py
@@ -1,7 +1,6 @@
-
-from datetime import datetime
 import logging
 import os
+from datetime import datetime
 
 DATE_FORMAT = '%d-%m-%Y'
 SUCESS_FILENAME = 'success_logs/ProjectName-{}.log'
@@ -9,17 +8,15 @@ ERROR_FILENAME = 'error_logs/ProjectName-{}.log'
 DIRECTORY_SUCCESS = "success_logs/"
 DIRECTORY_ERROR = "error_logs/"
 FORMAT_LOGGING = '%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s:%(message)s'
-DATA_ADDED_LOG = 'data is added to database '
-DATA_ERROR_LOG = 'data is not to database '
+
 DATE_TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 
-def set_success_logger():
+def log_this_logger(exe):
     """
-    This will create the sucess logs.
+    This will log the message from the exe variable
     :return: Null
     """
-
 
     today = datetime.today().strftime(DATE_FORMAT)
     filename = SUCESS_FILENAME.format(today)
@@ -33,25 +30,4 @@ def set_success_logger():
         datefmt=DATE_TIME_FORMAT
     )
 
-    logging.debug(DATA_ADDED_LOG)
-
-
-def set_error_logger():
-    """
-    This will create the error logs.
-    :return: Null
-    """
-
-
-    today = datetime.today().strftime(DATE_FORMAT)
-    filename = ERROR_FILENAME.format(today)
-
-    if not os.path.exists(DIRECTORY_ERROR):
-        os.makedirs(DIRECTORY_ERROR)
-
-    logging.basicConfig(
-        filename=filename, level=logging.DEBUG,
-        format=FORMAT_LOGGING,
-        datefmt=DATE_TIME_FORMAT
-    )
-    logging.debug(DATA_ERROR_LOG)
+    logging.debug(exe)
